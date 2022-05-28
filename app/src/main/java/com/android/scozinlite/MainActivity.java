d45bab8ad90eaedd27904d00193cf917b543d928
mshaheerz <mshaheerkp9@gmail.com>
2022-05-28 16:38:36 +0530

    first commit

---
+++ app/src/main/java/com/android/scozinlite/MainActivity.java
@@ -0,0 +1,28 @@
+package com.android.scozinlite;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(MainActivity.this, WebActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
+    }
+}
